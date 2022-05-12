c33f6cfcb311562fe11b294f04522e344d30add8
ArnabShikder24 <arnabshikder2001@gmail.com>
2022-05-12 18:43:49 +0600

    footer polis

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,35 +2,41 @@ import React from 'react';
 import footer from '../../assets/images/footer.png';
 
 const Footer = () => {
+    const date = new Date();
+    const year = date.getFullYear();
     return (
         <footer style={{
             background: `url(${footer})`,
             backgroundSize: 'cover'
         }} className="p-10">
-            <div className='footer'>
+            <div className='flex justify-center'>
                 <div>
-                    <span className="footer-title">Services</span>
-                    <a href='/' className="link link-hover">Branding</a>
-                    <a href='/' className="link link-hover">Design</a>
-                    <a href='/' className="link link-hover">Marketing</a>
-                    <a href='/' className="link link-hover">Advertisement</a>
+                    <div className='footer'>
+                        <div>
+                            <span className="footer-title">Services</span>
+                            <a href='/' className="link link-hover">Branding</a>
+                            <a href='/' className="link link-hover">Design</a>
+                            <a href='/' className="link link-hover">Marketing</a>
+                            <a href='/' className="link link-hover">Advertisement</a>
+                        </div>
+                        <div className=' md:mx-52 lg:mx-96'>
+                            <span className="footer-title">Company</span>
+                            <a href='/' className="link link-hover">About us</a>
+                            <a href='/' className="link link-hover">Contact</a>
+                            <a href='/' className="link link-hover">Jobs</a>
+                            <a href='/' className="link link-hover">Press kit</a>
+                        </div>
+                        <div>
+                            <span className="footer-title">Legal</span>
+                            <a href='/' className="link link-hover">Terms of use</a>
+                            <a href='/' className="link link-hover">Privacy policy</a>
+                            <a href='/' className="link link-hover">Cookie policy</a>
+                        </div>
+                    </div>
+                    <div className='my-10 text-center'>
+                        <p>Copyright © {year} - All right reserved</p>
+                    </div>
                 </div>
-                <div>
-                    <span className="footer-title">Company</span>
-                    <a href='/' className="link link-hover">About us</a>
-                    <a href='/' className="link link-hover">Contact</a>
-                    <a href='/' className="link link-hover">Jobs</a>
-                    <a href='/' className="link link-hover">Press kit</a>
-                </div>
-                <div>
-                    <span className="footer-title">Legal</span>
-                    <a href='/' className="link link-hover">Terms of use</a>
-                    <a href='/' className="link link-hover">Privacy policy</a>
-                    <a href='/' className="link link-hover">Cookie policy</a>
-                </div>
-            </div>
-            <div className='my-10 text-center'>
-                <p>Copyright © 2022 - All right reserved by ACME Industries Ltd</p>
             </div>
         </footer>
     );
